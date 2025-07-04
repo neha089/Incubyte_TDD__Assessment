d939f5d0adad10feb171964e5b987cadd5ce88cb
neha0890 <nehapatel30325@gmail.com>
2025-07-04 10:17:32 +0530

    [Refactor-2] Remove redundant conditions and directly sum all comma-separated single value numbers

--- src/main/java/com/neha/StringCalculator.java
+++ src/main/java/com/neha/StringCalculator.java
@@ -8,8 +8,6 @@ public class StringCalculator {
         if(intput_string.isEmpty()) return 0;
         String [] numbers=intput_string.split(",");
         int Sum=0;
-        if(numbers.length==1) return parseInt(numbers[0]);
-        if(numbers.length==2) return parseInt(numbers[0])+parseInt(numbers[1]);
         for(String number:numbers){
             Sum+=parseInt(number);
         }
