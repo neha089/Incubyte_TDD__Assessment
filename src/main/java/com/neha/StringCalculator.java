0e9d97348a4cf930080e5aa6f8778397328ada1a
neha0890 <nehapatel30325@gmail.com>
2025-07-04 10:15:04 +0530

    [Refactor-1] Simplify logic: use split array length to determine return value instead of string length

--- src/main/java/com/neha/StringCalculator.java
+++ src/main/java/com/neha/StringCalculator.java
@@ -5,12 +5,11 @@ import static java.lang.Integer.parseInt;
 public class StringCalculator {
 
     public static int add(String intput_string) {
-        int length_of_string=intput_string.length();
-        if(length_of_string==0) return 0;
+        if(intput_string.isEmpty()) return 0;
         String [] numbers=intput_string.split(",");
         int Sum=0;
-        if(length_of_string==1) return parseInt(intput_string);
-        if(length_of_string==3) return parseInt(numbers[0])+parseInt(numbers[1]);
+        if(numbers.length==1) return parseInt(numbers[0]);
+        if(numbers.length==2) return parseInt(numbers[0])+parseInt(numbers[1]);
         for(String number:numbers){
             Sum+=parseInt(number);
         }
