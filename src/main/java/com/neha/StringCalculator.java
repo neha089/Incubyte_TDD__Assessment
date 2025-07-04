308d34fed19820908881c4df62772234e8692493
neha0890 <nehapatel30325@gmail.com>
2025-07-04 09:03:01 +0530

    [Step-1] Add and Pass test: empty string returns 0

--- src/main/java/com/neha/StringCalculator.java
+++ src/main/java/com/neha/StringCalculator.java
@@ -2,4 +2,8 @@ package com.neha;
 
 public class StringCalculator {
 
+    public static int add(String s) {
+
+        return 0;
+    }
 }
--- src/test/java/com/neha/StringCalculatorTest.java
+++ src/test/java/com/neha/StringCalculatorTest.java
@@ -2,9 +2,12 @@ package com.neha;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
     @Test
-    public void nothing() {
+    public void shouldReturnZeroForEmptyString() {
+        assertEquals(0,StringCalculator.add(""));
 
     }
 
