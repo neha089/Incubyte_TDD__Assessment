f35d2030e8779f49e6738ef168114942dc3fb4ed
neha0890 <nehapatel30325@gmail.com>
2025-07-04 10:29:29 +0530

    [Step-6] Add failing test: multiple comma-separated numbers with newline delimiter should return correct sum

--- src/test/java/com/neha/StringCalculatorTest.java
+++ src/test/java/com/neha/StringCalculatorTest.java
@@ -8,30 +8,27 @@ public class StringCalculatorTest {
     @Test
     public void shouldReturnZeroForEmptyString() {
         assertEquals(0,StringCalculator.add(""));
-
     }
     @Test
     public void shouldReturnNumberForSingleInput(){
         assertEquals(1,StringCalculator.add("1"));
         assertEquals(30,StringCalculator.add("30"));
-
     }
     @Test
     public void shouldReturnSumOfTwoCommaSeparatedNumbers(){
         assertEquals(3,StringCalculator.add("1,2"));
         assertEquals(30,StringCalculator.add("10,20"));
         assertEquals(300,StringCalculator.add("100,200"));
-
-
-
     }
     @Test
     public void shouldReturnSumOfMultipleCommaSeparatedNumbers(){
         assertEquals(6,StringCalculator.add("1,2,3"));
         assertEquals(69,StringCalculator.add("10,50,9"));
         assertEquals(699,StringCalculator.add("100,500,99"));
-
-
+    }
+    @Test
+    public void shouldHandleNewlineAsDelimiter(){
+        assertEquals(20,StringCalculator.add("10\n2,8"));
     }
 
 
