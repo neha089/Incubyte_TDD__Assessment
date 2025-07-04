bfd32fb6d6b28d893342735ea123c0a3558d2b3e
neha0890 <nehapatel30325@gmail.com>
2025-07-04 12:13:28 +0530

    [Refactor-3] simplify StringCalculator by unifying delimiter logic and improving readability

--- src/main/java/com/neha/StringCalculator.java
+++ src/main/java/com/neha/StringCalculator.java
@@ -7,22 +7,18 @@ public class StringCalculator {
     public static int add(String input_string) {
         if(input_string.isEmpty()) return 0;
         int Sum=0;
-        if(input_string.startsWith("//")){
-            int index_of_newline=input_string.indexOf("\n");
-            String delimiter=input_string.substring(2,index_of_newline);
-            input_string=input_string.substring(index_of_newline+1);
-            String numbers1[]=input_string.split(delimiter);
-            for(String number:numbers1){
-                Sum+=parseInt(number);
-            }
+        String[] numbers;
+        String delimiter=",|\n";
+        String string_of_number=input_string;
+        if(string_of_number.startsWith("//")){
+            int index_of_newline=string_of_number.indexOf("\n");
+            delimiter=string_of_number.substring(2,index_of_newline);
+            string_of_number=string_of_number.substring(index_of_newline+1);
         }
-        else {
-            String[] numbers = input_string.split("[,\\n]");
-
+        numbers=string_of_number.split(delimiter);
             for (String number : numbers) {
                 Sum += parseInt(number);
             }
-        }
         return Sum;
     }
 }
