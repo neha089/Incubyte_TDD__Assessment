92c69f1c9ed91d54710e1b1085f54be421953986
neha0890 <nehapatel30325@gmail.com>
2025-07-04 11:50:39 +0530

    [Step-7] Pass test: multiple comma-separated numbers with any delimiter should return correct sum

--- src/main/java/com/neha/StringCalculator.java
+++ src/main/java/com/neha/StringCalculator.java
@@ -4,12 +4,24 @@ import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
 
-    public static int add(String intput_string) {
-        if(intput_string.isEmpty()) return 0;
-        String [] numbers=intput_string.split("[,\\n]");
+    public static int add(String input_string) {
+        if(input_string.isEmpty()) return 0;
         int Sum=0;
-        for(String number:numbers){
-            Sum+=parseInt(number);
+        if(input_string.startsWith("//")){
+            int index_of_newline=input_string.indexOf("\n");
+            char delimiter=input_string.charAt(2);
+            input_string=input_string.substring(index_of_newline+1);
+            String numbers1[]=input_string.split(String.valueOf(delimiter));
+            for(String number:numbers1){
+                Sum+=parseInt(number);
+            }
+        }
+        else {
+            String[] numbers = input_string.split("[,\\n]");
+
+            for (String number : numbers) {
+                Sum += parseInt(number);
+            }
         }
         return Sum;
     }
