356129a031bf5d6c3cab5162b47d76804844b404
neha0890 <nehapatel30325@gmail.com>
2025-07-04 09:14:58 +0530

    [Step-2] Pass test: single number returns its value from string to integer

--- src/main/java/com/neha/StringCalculator.java
+++ src/main/java/com/neha/StringCalculator.java
@@ -1,9 +1,12 @@
 package com.neha;
 
+import static java.lang.Integer.parseInt;
+
 public class StringCalculator {
 
-    public static int add(String s) {
-
+    public static int add(String intput_string) {
+        int length_of_string=intput_string.length();
+        if(length_of_string==1) return parseInt(intput_string);
         return 0;
     }
 }
